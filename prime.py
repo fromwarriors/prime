999dc88281f36596e50ecd7b67287c27987b755c
Michael Oselskyi <oselskyi@gmail.com>
2013-08-10 01:50:19 -0700

    Update prime.py
    added loop function.

--- prime.py
+++ prime.py
@@ -1,10 +1,25 @@
+#!/usr/bin/env python
+
+import os
+
+def _clear_screen():
+    os.system(['clear','cls'][os.name=='nt'])
+
 def prime(x):
-    if(n == 1):
-        return 'composite'
-    num = 2
-    while x > num:
-        if x % num == 0:
-            return 'composite'
-        else:
-            num += 1
-    return 'prime'
+    x = abs(int(x))
+    if x < 2:
+        return True
+    if not x & 1:
+        return False
+    for n in range(3, int(x**0.5)+1, 2):
+         if x % n ==0:
+             return False
+    return True
+_clear_screen()
+def loop():
+	print "Prime numbers. True=prime, False=composite.CTRL=D=exit" 
+	x = 1
+	while x == 1:
+		num = input("Input number: ")
+		print prime(num)
+loop()
